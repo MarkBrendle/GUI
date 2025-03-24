c20a8b858c594d02311b59c6a33176b17b807e16
focos <Markbrendle5@gmail.com>
2025-03-24 09:54:30 -0400

    made a basic GUI

--- coding/Start.java
+++ coding/Start.java
@@ -1,3 +1,28 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Start {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Start");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Welcome to the game!");
+        JButton button = new JButton("Start");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
 }
